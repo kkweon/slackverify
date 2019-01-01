6ea4a14f67345e752fe50736d18ad69e99e9b003
Kyung Mo Kweon <kkweon@gmail.com>
2018-12-31 21:31:36 -0800

    Add 'VerifyRequest'

--- slackverify.go
+++ slackverify.go
@@ -6,6 +6,9 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
 )
 
 // Verify verifies Slack Request
@@ -29,4 +32,34 @@ func Verify(token []byte, body []byte, timestamp int64, expectedHex []byte) bool
 	}
 
 	return hmac.Equal(code, expected)
+}
+
+// VerifyRequest is a wrapper of 'Verify' but does with '*http.Request'
+func VerifyRequest(r *http.Request, token []byte) bool {
+	ts := r.Header.Get("X-Slack-Request-Timestamp")
+	expectedHex := r.Header.Get("X-Slack-Signature")
+
+	if ts == "" || expectedHex == "" {
+		return false
+	}
+
+	timestamp, err := strconv.ParseInt(ts, 10, 64)
+
+	if err != nil {
+		return false
+	}
+
+	bodyR, err := r.GetBody()
+
+	if err != nil {
+		return false
+	}
+
+	body, err := ioutil.ReadAll(bodyR)
+
+	if err != nil {
+		return false
+	}
+
+	return Verify(token, body, timestamp, []byte(expectedHex))
 }
