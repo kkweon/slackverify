69d3033aa537c6823ab3f81c7aff1d903e7d858f
Kyung Mo Kweon <kkweon@gmail.com>
2019-01-02 22:06:19 -0800

    Add more test cases

--- slackverify_test.go
+++ slackverify_test.go
@@ -20,6 +20,18 @@ func TestVerifyWithoutVersionNumber(t *testing.T) {
 	}
 }
 
+func TestVerifyFail(t *testing.T) {
+	slackSigningToken := []byte("8f742231b10e8888abcd99yyyzzz85a5")
+	body := []byte("token=xyzz0WbapA4vBCDEFasx0q6G&team_id=T1DC2JH3J&team_domain=testteamnow&channel_id=G8PSS9T3V&channel_name=foobar&user_id=U2CERLKJA&user_name=roadrunner&command=%2Fwebhook-collect&text=&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT1DC2JH3J%2F397700885554%2F96rGlfmibIGlgcZRskXaIFfN&trigger_id=398738663015.47445629121.803a0bc887a14d10d2c447fce8b6703c")
+	var timestamp int64 = 1531420618
+
+	expectedHex := []byte("this should fail")
+
+	if ok := Verify(slackSigningToken, body, timestamp, expectedHex); ok {
+		t.Fatal("This should not be verified")
+	}
+}
+
 func TestVerifyRequest(t *testing.T) {
 	bodyRaw := "token=xyzz0WbapA4vBCDEFasx0q6G&team_id=T1DC2JH3J&team_domain=testteamnow&channel_id=G8PSS9T3V&channel_name=foobar&user_id=U2CERLKJA&user_name=roadrunner&command=%2Fwebhook-collect&text=&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT1DC2JH3J%2F397700885554%2F96rGlfmibIGlgcZRskXaIFfN&trigger_id=398738663015.47445629121.803a0bc887a14d10d2c447fce8b6703c"
 	body := bytes.NewBufferString(bodyRaw)
@@ -37,15 +49,26 @@ func TestVerifyRequest(t *testing.T) {
 	}
 }
 
-func TestVerifyFail(t *testing.T) {
-	slackSigningToken := []byte("8f742231b10e8888abcd99yyyzzz85a5")
-	body := []byte("token=xyzz0WbapA4vBCDEFasx0q6G&team_id=T1DC2JH3J&team_domain=testteamnow&channel_id=G8PSS9T3V&channel_name=foobar&user_id=U2CERLKJA&user_name=roadrunner&command=%2Fwebhook-collect&text=&response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FT1DC2JH3J%2F397700885554%2F96rGlfmibIGlgcZRskXaIFfN&trigger_id=398738663015.47445629121.803a0bc887a14d10d2c447fce8b6703c")
-	var timestamp int64 = 1531420618
+func TestFailVerifyRequest(t *testing.T) {
+	type testCase struct {
+		request *http.Request
+	}
+	testCases := make([]testCase, 0)
 
-	expectedHex := []byte("this should fail")
+	// no timestamp
+	req, _ := http.NewRequest("POST", "/api/doesn'tmatter", nil)
+	testCases = append(testCases, testCase{req})
 
-	if ok := Verify(slackSigningToken, body, timestamp, expectedHex); ok {
-		t.Fatal("This should not be verified")
+	// bad timestamp
+	req, _ = http.NewRequest("POST", "/api/doesn'tmatter", nil)
+	req.Header.Add("X-Slack-Request-Timestamp", "Not a timestamp")
+	req.Header.Add("X-Slack-Signature", "slack-signature")
+	testCases = append(testCases, testCase{req})
+
+	for _, testCase := range testCases {
+		if VerifyRequest(testCase.request, []byte("8f742231b10e8888abcd99yyyzzz85a5")) {
+			t.Fatal("It should fail")
+		}
 	}
 }
 
